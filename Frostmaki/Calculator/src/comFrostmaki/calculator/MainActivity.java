1e68a9f19fe5b7c4c013fe044fdd6c876c066ed6
Frostmaki <stephen.miao1219@gmail.com>
2016-03-02 22:17:54 +0800

    Second Commit

--- Frostmaki/Calculator/src/comFrostmaki/calculator/MainActivity.java
+++ Frostmaki/Calculator/src/comFrostmaki/calculator/MainActivity.java
@@ -27,37 +27,91 @@ public class MainActivity extends Activity implements OnClickListener{
 	Button but_del=(Button)findViewById(R.id.but_del);
 	Button but_clear=(Button)findViewById(R.id.but_clear);
 	
-	Button but_add=(Button)findViewById(R.id.but_add);
-	Button but_minus=(Button)findViewById(R.id.but_minus);
-	Button but_multiplied=(Button)findViewById(R.id.but_multiplied);
-	Button but_divided=(Button)findViewById(R.id.but_divided);
+	Button but_add=(Button)findViewById(R.id.but_add);//+
+	Button but_minus=(Button)findViewById(R.id.but_minus);//-
+	Button but_multiplied=(Button)findViewById(R.id.but_multiplied);//*
+	Button but_divided=(Button)findViewById(R.id.but_divided);// /
 	
 	Button but_equal=(Button)findViewById(R.id.but_equal);
 	
 	Button but_point=(Button)findViewById(R.id.but_point);
 	
+	public String inputStr;
+	public String outputStr;
 	
-	private String str="";
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        but_one.setOnClickListener(this);
+        but_two.setOnClickListener(this);
+        but_three.setOnClickListener(this);
+        but_four.setOnClickListener(this);
+        but_five.setOnClickListener(this);
+        but_six.setOnClickListener(this);
+        but_seven.setOnClickListener(this);
+        but_nine.setOnClickListener(this);
+        but_eight.setOnClickListener(this);
+        but_zero.setOnClickListener(this);
+        but_point.setOnClickListener(this);
         
+        but_del.setOnClickListener(this);
+        but_clear.setOnClickListener(this);
         
+        but_add.setOnClickListener(this);
+        but_minus.setOnClickListener(this);
+        but_multiplied.setOnClickListener(this);
+        but_divided.setOnClickListener(this);
+        
+        but_equal.setOnClickListener(this);
+
     }
 
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
-		case :
+		case R.id.but_one:
+		case R.id.but_two:
+		case R.id.but_three:
+		case R.id.but_four:
+		case R.id.but_five:
+		case R.id.but_six:
+		case R.id.but_seven:
+		case R.id.but_eight:
+		case R.id.but_nine:
+		case R.id.but_zero:
+		case R.id.but_point:
 			
+			inputStr=inputStr+((Button)v).getText().toString();
+			inputText.setText(inputStr);
 			break;
-
+		case R.id.but_add:
+		case R.id.but_minus:
+		case R.id.but_multiplied:
+		case R.id.but_divided:
+			inputStr=inputStr+" "+((Button)v).getText().toString()+" ";
+			inputText.setText(inputStr);
+			break;
+		case R.id.but_del:
+			inputStr=inputStr.substring(0, inputStr.length()-1);
+			inputText.setText(inputStr);
+			break;
+		case R.id.but_clear:
+			inputStr="";
+			inputText.setText(inputStr);
+			break;
+		case R.id.but_equal:
+		    OutputText.setText(outputStr);
+			break;
 		default:
 			break;
 		}
+	}
+	
+	public void getResult(){
+		
 	}
     
     
