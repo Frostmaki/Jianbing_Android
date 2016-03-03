e69d3fd45fa4613a7ee04e2a40d55c347697b680
Frostmaki <stephen.miao1219@gmail.com>
2016-03-03 22:38:34 +0800

    Third Commit

--- Frostmaki/Calculator/src/comFrostmaki/calculator/MainActivity.java
+++ Frostmaki/Calculator/src/comFrostmaki/calculator/MainActivity.java
@@ -2,6 +2,7 @@ package comFrostmaki.calculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -10,40 +11,69 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnClickListener{
 
-	TextView inputText =(TextView)findViewById(R.id.textInput);
-	TextView OutputText =(TextView)findViewById(R.id.textOnput);
+	TextView inputText;
+	TextView OutputText;
 	
-	Button but_zero=(Button)findViewById(R.id.but_zero);
-	Button but_one=(Button)findViewById(R.id.but_one);
-	Button but_two=(Button)findViewById(R.id.but_two);
-	Button but_three=(Button)findViewById(R.id.but_three);
-	Button but_four=(Button)findViewById(R.id.but_four);
-	Button but_five=(Button)findViewById(R.id.but_five);
-	Button but_six=(Button)findViewById(R.id.but_six);
-	Button but_seven=(Button)findViewById(R.id.but_seven);
-	Button but_eight=(Button)findViewById(R.id.but_eight);
-	Button but_nine=(Button)findViewById(R.id.but_nine);
+	Button but_zero;
+	Button but_one;
+	Button but_two;
+	Button but_three;
+	Button but_four;
+	Button but_five;
+	Button but_six;
+	Button but_seven;
+	Button but_eight;
+	Button but_nine;
 	
-	Button but_del=(Button)findViewById(R.id.but_del);
-	Button but_clear=(Button)findViewById(R.id.but_clear);
+	Button but_del;
+	Button but_clear;
 	
-	Button but_add=(Button)findViewById(R.id.but_add);//+
-	Button but_minus=(Button)findViewById(R.id.but_minus);//-
-	Button but_multiplied=(Button)findViewById(R.id.but_multiplied);//*
-	Button but_divided=(Button)findViewById(R.id.but_divided);// /
+	Button but_add;//+
+	Button but_minus;//-
+	Button but_multiplied;//*
+	Button but_divided;// /
 	
-	Button but_equal=(Button)findViewById(R.id.but_equal);
+	Button but_equal;
 	
-	Button but_point=(Button)findViewById(R.id.but_point);
+	Button but_point;
 	
-	public String inputStr;
-	public String outputStr;
+	public String inputStr;    
+	
 	
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    	inputText =(TextView)findViewById(R.id.textInput);
+    	OutputText =(TextView)findViewById(R.id.textOnput);
+    	
+    	but_zero=(Button)findViewById(R.id.but_zero);
+    	but_one=(Button)findViewById(R.id.but_one);
+    	but_two=(Button)findViewById(R.id.but_two);
+    	but_three=(Button)findViewById(R.id.but_three);
+    	but_four=(Button)findViewById(R.id.but_four);
+    	but_five=(Button)findViewById(R.id.but_five);
+    	but_six=(Button)findViewById(R.id.but_six);
+    	but_seven=(Button)findViewById(R.id.but_seven);
+    	but_eight=(Button)findViewById(R.id.but_eight);
+    	but_nine=(Button)findViewById(R.id.but_nine);
+    	
+    	but_del=(Button)findViewById(R.id.but_del);
+    	but_clear=(Button)findViewById(R.id.but_clear);
+    	
+    	but_add=(Button)findViewById(R.id.but_add);//+
+    	but_minus=(Button)findViewById(R.id.but_minus);//-
+    	but_multiplied=(Button)findViewById(R.id.but_multiplied);//*
+    	but_divided=(Button)findViewById(R.id.but_divided);// /
+    	
+    	but_equal=(Button)findViewById(R.id.but_equal);
+    	
+    	but_point=(Button)findViewById(R.id.but_point);
+    	
+    	inputStr=inputText.getText().toString();
+    	
+    	
         but_one.setOnClickListener(this);
         but_two.setOnClickListener(this);
         but_three.setOnClickListener(this);
@@ -65,12 +95,11 @@ public class MainActivity extends Activity implements OnClickListener{
         but_divided.setOnClickListener(this);
         
         but_equal.setOnClickListener(this);
-
+        Log.d("cuowu", "button");
     }
 
 	@Override
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
 		switch (v.getId()) {
 		case R.id.but_one:
 		case R.id.but_two:
@@ -86,6 +115,7 @@ public class MainActivity extends Activity implements OnClickListener{
 			
 			inputStr=inputStr+((Button)v).getText().toString();
 			inputText.setText(inputStr);
+			Log.d("cuowu", "123456789");
 			break;
 		case R.id.but_add:
 		case R.id.but_minus:
@@ -93,24 +123,61 @@ public class MainActivity extends Activity implements OnClickListener{
 		case R.id.but_divided:
 			inputStr=inputStr+" "+((Button)v).getText().toString()+" ";
 			inputText.setText(inputStr);
+			Log.d("cuowu", "+-*/");
 			break;
 		case R.id.but_del:
-			inputStr=inputStr.substring(0, inputStr.length()-1);
-			inputText.setText(inputStr);
+			
+				if(inputStr.substring(inputStr.length())!=" "){
+			       inputStr=inputStr.substring(0, inputStr.length()-1);
+			       
+				}else{
+					inputStr=inputStr.substring(0, inputStr.length()-2);
+				}
+				inputText.setText(inputStr);
+			
+			Log.d("cuowu", "del");
 			break;
 		case R.id.but_clear:
 			inputStr="";
 			inputText.setText(inputStr);
+			Log.d("cuowu", "clear");
 			break;
 		case R.id.but_equal:
-		    OutputText.setText(outputStr);
+			getResult();
+			Log.d("cuowu", "equal");
 			break;
-		default:
-			break;
+		
 		}
 	}
 	
 	public void getResult(){
+		int int0=0;
+		int int1 =inputStr.indexOf(" ");//第一个空格
+		String str2 =inputStr.substring(0, int1);//前一个数字
+		double d2=Double.parseDouble(str2);
+		int int3 =inputStr.indexOf(" ", int1+2);//再往后一个空格
+		String str4 =inputStr.substring(int1+3);//后一个数字
+		double d4=Double.parseDouble(str4);
+		String str5 =inputStr.substring(int1+1);   //算数运算符
+		//int0=int0+2;
+		double d6 = 0;
+		if (str5.equals("+")){
+			d6=d2 + d4;
+		}else if(str5.equals("-")){
+			d6=d2 - d4;
+		}else if(str5.equals("*")){
+			d6=d2 * d4;
+		}else if(str5.equals("/")){
+			if(d4==0)
+			{
+				d6=0;
+			}else
+			{
+			    d6=d2 / d4;
+			}
+		}
+		
+		OutputText.setText((int)d6+"");	
 		
 	}
     
